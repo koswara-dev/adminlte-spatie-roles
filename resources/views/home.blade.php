510d370f73479786106b87222ad3517acd0aad8b
Koswara Dev <koswara.c79@gmail.com>
2021-07-13 23:19:48 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">You are logged in <b>
+                        @role('admin')
+                        Admin
+                        @else
+                        User
+                        @endrole
+                        </b> role!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
